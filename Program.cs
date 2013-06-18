6a3a14383df9e0cb4ee2f5783056e7b0fa55a8a0
davidh-arcsoftware <david.honess@arcsoftwareconsultancy.com>
2013-06-18 11:34:39 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello this is a test");
+            Console.ReadKey();
+        }
+    }
+}
